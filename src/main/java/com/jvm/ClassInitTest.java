d04e736ab9278661f5eaa773a426f2e09f65c06a
Jinhua-Lee <Jinhua-Lee@outlook.com>
2020-10-11 22:12:17 +0800

    类加载过程模拟类.

--- src/main/java/com/jvm/ClassInitTest.java
+++ src/main/java/com/jvm/ClassInitTest.java
@@ -12,9 +12,32 @@ package com.jvm;
  * @date 2020/9/30 21:59
  */
 public class ClassInitTest {
+
+    /**
+     * Linking：Prepare阶段 num = 1
+     * Initial：num = 1 -> num = 2
+     */
     private static int num = 1;
 
-    public static void main(String[] args) {
+    static {
+        num = 2;
+        number = 20;
         System.out.println("num = " + num);
+        // 报错，非法前向引用
+//        System.out.println("number = " + number);
+    }
+
+    /**
+     * Linking：Prepare阶段 number = 0;
+     * Initial: number = 20 -> number = 10(源文件中的顺序)
+     */
+    private static int number = 10;
+
+    public static void main(String[] args) {
+        // num == 10
+        System.out.println("num = " + num);
+
+        // number == 20
+        System.out.println("number = " + number);
     }
 }
