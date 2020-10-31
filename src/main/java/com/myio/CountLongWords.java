11ad60b0ce70b3e64acdcfb09050e333c443856f
Jinhua-Lee <Jinhua-Lee@outlook.com>
2020-10-31 13:46:36 +0800

    flatMap和map的演示.

--- src/main/java/com/myio/CountLongWords.java
+++ src/main/java/com/myio/CountLongWords.java
@@ -16,42 +16,42 @@ import java.util.stream.Stream;
  * @author Jinhua
  */
 public class CountLongWords {
-	public static void main(String[] args) throws IOException {
-		String dir = "E:/IOTest/";
-		String filename = "chars.txt";
-		String contents = new String(Files.readAllBytes(Paths.get(dir + filename)), StandardCharsets.UTF_8);
-		List<String> words = Arrays.asList(contents.split("\\PL+"));
+    public static void main(String[] args) throws IOException {
+        String dir = "E:/IOTest/";
+        String filename = "chars.txt";
+        String contents = new String(Files.readAllBytes(Paths.get(dir + filename)), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("\\PL+"));
 
-		long count = 0;
-		for (String s : words) {
-			if(s.length() > 4) {
-				count++;
-			}
-		}
-		System.out.println(count);
+        long count = 0;
+        for (String s : words) {
+            if(s.length() > 4) {
+                count++;
+            }
+        }
+        System.out.println(count);
 
-		count = words.stream().filter(s -> s.length() > 4).count();
-		System.out.println(count);
+        count = words.stream().filter(s -> s.length() > 4).count();
+        System.out.println(count);
 
-		count = words.parallelStream().filter(s -> s.length() > 4).count();
-		System.out.println(count);
-	}
+        count = words.parallelStream().filter(s -> s.length() > 4).count();
+        System.out.println(count);
+    }
 
-	@Test
-	public void testStream() {
-		List<String> words = new ArrayList<String>();
-		words.add("your");
-		words.add("name");
+    @Test
+    public void testStream() {
+        List<String> words = new ArrayList<String>();
+        words.add("your");
+        words.add("name");
 
-		Stream<Stream<Character>> result = words.stream().map(CountLongWords::characterStream);
-		Stream<Character> letters = words.stream().flatMap(CountLongWords::characterStream);
-	}
+        Stream<Stream<Character>> result = words.stream().map(CountLongWords::characterStream);
+        Stream<Character> letters = words.stream().flatMap(CountLongWords::characterStream);
+    }
 
-	public static Stream<Character> characterStream(String s){
-		List<Character> result = new ArrayList<>();
-		for (char c : s.toCharArray()) {
-			result.add(c);
-		}
-		return result.stream();
-	}
+    public static Stream<Character> characterStream(String s){
+        List<Character> result = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            result.add(c);
+        }
+        return result.stream();
+    }
 }
