321572732b2befad8a5abddd5a0f0971d6916c62
Jinhua-Lee <Jinhua-Lee@outlook.com>
2020-11-02 00:15:38 +0800

    插入排序和希尔排序的梳理.

--- src/main/java/com/sort/MySort.java
+++ src/main/java/com/sort/MySort.java
@@ -16,55 +16,37 @@ public class MySort {
     }
 
     public void test() {
-        int[] a = {32, 43, 23, 13, 5};
-//		insertSort(a);
-        bubbleSort(a);
-//		quickSort(a, 0, a.length - 1);
-//		swapSort(a);
-        for (int i : a) {
+        int[] arr = {32, 43, 23, 13, 5};
+//		insertSort(arr);
+//        bubbleSort(arr);
+//		quickSort(arr, 0, arr.length - 1);
+//		swapSort(arr);
+        shellSort(arr);
+        for (int i : arr) {
             System.out.printf("%-4d", i);
         }
         System.out.println();
     }
 
     /**
-     * 1. 直接插入排序
-     * @param a  待排序数组
+     * 01_冒泡排序
+     *      1) 每轮会选出最大的元素到末端
+     *      2) 改进：增加一个变量标记该轮是否有交换过元素，若没有交换，则已经排好序
+     * @param arr 待排序数组
      */
-    public void insertSort(int[] a) {
-        int length = a.length;
-        // 存放待插入数据的变量
-        int insertNum;
-        // 从第二位开始依次遍历插入
-        for (int i = 1; i < length; i++) {
-            insertNum = a[i];
-            int j = i - 1;
-            // 从前往后遍历大于insertNum的数，依次往后移动
-            while (j >= 0 && a[j] > insertNum) {
-                a[j + 1] = a[j];
-                j--;
-            }
-            a[j + 1] = insertNum;
-        }
-    }
-
-    /**
-     * 2. 冒泡排序
-     * @param a 待排序数组
-     */
-    public void bubbleSort(int[] a) {
+    public void bubbleSort(int[] arr) {
         int temp;
         // 外层表示循环次数，循环 length - 1次
-        for (int i = 0; i < a.length - 1; i++) {
-            // 该轮是否排交换过的标志变量，若没改变，则表示没进行过排序，已经排好
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 该轮是否排交换过的标志变量，若没改变，则表示没进行过交换，已经排好
             boolean swap =false;
             // 每一轮排序（外层），会将大的值排到数组末端， 所以内层每轮到的数组位置是越来越小（length - i - 1）
-            for (int j = 0; j < a.length - i - 1; j++) {
-                if (a[j] > a[j+1]) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
                     swap = true;
-                    temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
             if (!swap) {
@@ -74,79 +56,127 @@ public class MySort {
     }
 
     /**
-     * 3. 简单选择排序
-     * @param a 待排序数组
+     * 02_简单选择排序
+     * @param arr 待排序数组
      */
-    public void selectSort(int[] a) {
-        int len = a.length;
+    public void selectSort(int[] arr) {
+        int len = arr.length;
         // 外层循环：i即可表示，如果产生小值要放入的位置。
         for(int i = 0; i < len ; i++){
-            int value = a[i];
+            int value = arr[i];
             int position = i;
             // 选出小的值，记录小值的位置
             for(int j = i+1; j < len; j++){
-                if(a[j] < value){
-                    value = a[j];
+                if(arr[j] < value){
+                    value = arr[j];
                     position = j;
                 }
             }
             // 交换最小值和当前外层遍历的位置
-            a[position] = a[i];
-            a[i] = value;
+            arr[position] = arr[i];
+            arr[i] = value;
         }
     }
 
     /**
-     * 4. 快速排序
-     * @param a 待排序数组
-     * @param start
-     * @param end
+     * 03_直接插入排序
+     * @param arr  待排序数组
      */
-    public void quickSort(int[] a, int start, int end) {
+    public void insertSort(int[] arr) {
+        int length = arr.length;
+        // 存放待插入数据的变量
+        int insertNum;
+        // 从第二位开始依次遍历插入，第一个数不用插入
+        for (int i = 1; i < length; i++) {
+            insertNum = arr[i];
+            int j = i - 1;
+            // 从后往前倒序，遍历大于insertNum的数，依次往后移动。j最小值是0
+            while (j >= 0 && arr[j] > insertNum) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            // 出循环前j--，则要插入的位置，需要取 [j + 1] 位置
+            arr[j + 1] = insertNum;
+        }
+    }
+
+    /**
+     * 04_希尔排序
+     *		1) 改进的直接插入排序；
+     *		2) 增加了分组。按增量进行分组，组内仍使用直接插入排序
+     *		3) 组内的直接插入排序找前一个元素，从固定移动1，改为指定步长
+     * @param arr 待排序数组
+     */
+    public void shellSort(int[] arr){
+        // 单独把数组长度拿出来，提高效率
+        int length = arr.length;
+        while(length > 0){
+            // 增量公式，简单折半
+            length = length / 2;
+            // 第一层，是分组，每组执行插入排序
+            for(int i = 0; i < length; i++){
+                //	每组从第二个元素开始，按增量确定该组的元素
+                for(int j = i + length; j < arr.length; j+= length){
+                    // 要插入的元素
+                    int value = arr[j];
+                    // k为有序序列最后一位的位数
+                    int k = j - length;
+
+                    /*for (; k >= 0 && value < arr[k]; k -= length) {
+                        arr[k + length] = arr[k];
+                    }*/
+
+                    // 从后往前，如果大于待插入值，则该值向后移动
+                    while(k >= 0 && arr[k] > value) {
+                        arr[k + length] = arr[k];
+                        // 按步长向前移动
+                        k -= length;
+                    }
+                    // 出循环前，k减去length，所以真正插入的位置，需要加上length
+                    arr[k + length] = value;
+                }
+            }
+        }
+    }
+
+    /**
+     * 05_快速排序
+     * @param arr 待排序数组
+     * @param start 开始下标
+     * @param end 结束下标
+     */
+    public void quickSort(int[] arr, int start, int end) {
         if (start < end) {
             // 选基准值
-            int baseNum=a[start];
+            int baseNum= arr[start];
             // 记录中间值
             int midNum;
             int i=start;
             int j=end;
             do {
-                while((a[i] < baseNum) && i < end) {
+                while((arr[i] < baseNum) && i < end) {
                     i++;
                 }
-                while((a[j] > baseNum) && j > start) {
+                while((arr[j] > baseNum) && j > start) {
                     j--;
                 }
                 if(i <= j){
-                    midNum = a[i];
-                    a[i] = a[j];
-                    a[j] = midNum;
+                    midNum = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = midNum;
                     i++;
                     j--;
                 }
             } while (i<=j);
             if (start < j) {
-                quickSort(a, start, j);
+                quickSort(arr, start, j);
             }
             if (end > i){
-                quickSort(a, i, end);
+                quickSort(arr, i, end);
             }
         }
     }
 
-    /**
-     * 5. 交换排序
-     * @param a 待排序数组
-     */
-    public void swapSort(int[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
-    }
+
+
 }
