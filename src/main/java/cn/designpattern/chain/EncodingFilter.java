4986124bfc7a0039f736faa549efc2bd05759d56
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-03-10 23:47:17 +0800

    职责链模式-过滤器案例

---
+++ src/main/java/cn/designpattern/chain/EncodingFilter.java
@@ -0,0 +1,40 @@
+package cn.designpattern.chain;
+
+import org.apache.commons.io.Charsets;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ * 职责链模式 -> 过滤器案例
+ *
+ * @author Jinhua
+ * @date 2021/3/10下午11:18
+ */
+public class EncodingFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        request.setCharacterEncoding(Charset.defaultCharset().name());
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setContentType("text/html; charset=UTF-8");
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
