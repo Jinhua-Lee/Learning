aafe9fd45cb89fcebb3de13ca5781e11b5141e0b
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-03-16 22:47:02 +0800

    JVM笔记部分迁移

--- src/main/java/cn/jvm/natives/IHaveNatives.java
+++ src/main/java/cn/jvm/natives/IHaveNatives.java
@@ -8,21 +8,22 @@
 package cn.jvm.natives;
 
 /**
- * 本地方法定义的演示类。
- *      1. native修饰的方法都没有方法体。
- *      2. native和abstract关键字不能同用。
- *          a. native的方法实现体不是Java语言，但不表示没有。
- *          b. abstract方法是 虚方法 / 抽象方法。
+ * 本地方法定义的演示类。<p>
+ * 1. native修饰的方法都没有方法体。<p>
+ * 2. native和abstract关键字不能同用。<p>
+ * &emsp;a. native的方法实现体不是Java语言，但不表示没有。<p>
+ * &emsp;b. abstract方法是 虚方法 / 抽象方法。<p>
+ *
  * @author Jinhua
  * @date 2020/10/29 22:41
  */
 public class IHaveNatives {
 
-    public native void Native1(int x);
+    public native void native1(int x);
 
-    native static public long Native2();
+    native static public long native2();
 
-    native synchronized private float Native3(Object o);
+    native synchronized private float native3(Object o);
 
-    native void Native4(int[] array) throws Exception;
+    native void native4(int[] array) throws Exception;
 }
