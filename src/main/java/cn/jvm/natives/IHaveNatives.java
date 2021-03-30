262ca525bd636ac5d1b259909b02f40531d5297f
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-03-30 15:58:34 +0800

    本地方法相关笔记迁移

--- src/main/java/cn/jvm/natives/IHaveNatives.java
+++ src/main/java/cn/jvm/natives/IHaveNatives.java
@@ -3,9 +3,9 @@ package cn.jvm.natives;
 /**
  * 本地方法定义的演示类。<p>
  * 1. native修饰的方法都没有方法体。<p>
- * 2. native和abstract关键字不能同用。<p>
- * &emsp;a. native的方法实现体不是Java语言，但不表示没有。<p>
- * &emsp;b. abstract方法是 虚方法 / 抽象方法。<p>
+ * 2. native和abstract关键字不能同用。<p>&emsp;
+ * a. native的方法实现体不是Java语言，但不表示没有。<p>&emsp;
+ * b. abstract方法是 虚方法 / 抽象方法。<p>&emsp;
  *
  * @author Jinhua
  * @date 2020/10/29 22:41
