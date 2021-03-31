f1aa7d701480fe5a5e74e0ead8da1ff018d140ca
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-03-31 11:38:42 +0800

    lombok目前还不支持JDK16的record类

---
+++ src/main/java/com/se/valobj/Point.java
@@ -0,0 +1,16 @@
+package com.se.valobj;
+
+/**
+ * 二维坐标点
+ *
+ * @author Jinhua
+ * @version 1.0
+ * @date 2021/3/31 10:58
+ */
+//public record Point(int x, int y) {
+//    public static void main(String[] args) {
+//        Point p1 = new Point(1, 2);
+//        Point p2 = new Point(1, 2);
+//        System.out.println(p1.equals(p2));
+//    }
+//}
