5620d82ae6411d73ebbe76ce88601c06ece034e6
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-04-13 13:58:50 +0800

    something

--- src/main/java/cn/jvm/runtime/methodarea/MethodAreaOom.java
+++ src/main/java/cn/jvm/runtime/methodarea/MethodAreaOom.java
@@ -1,11 +1,11 @@
 package cn.jvm.runtime.methodarea;
 
-import jdk.internal.org.objectweb.asm.ClassWriter;
-import jdk.internal.org.objectweb.asm.Opcodes;
+import org.springframework.asm.ClassWriter;
+import org.springframework.asm.Opcodes;
 
 /**
  * 方法区内存溢出的例子<p>&emsp;
- * 1) VM参数：-XX:MetaspaceSize=10M -XXMaxMetaspaceSize=10M<p>&emsp;
+ * 1) VM参数：-XX:MetaspaceSize=10M -XX:MaxMetaspaceSize=10M<p>&emsp;
  * 2) 实现：利用ClassWriter
  *
  * @author Jinhua
@@ -22,7 +22,8 @@ public class MethodAreaOom extends ClassLoader {
                 // 用于生成类的二进制字节码
                 ClassWriter clWriter = new ClassWriter(0);
                 // 版本号，修饰符，类名，包名，父类，接口
-                clWriter.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, "Class" + i, null, "java/lang/Object", null);
+                clWriter.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, "Class" + i,
+                        null, "java/lang/Object", null);
                 byte[] code = clWriter.toByteArray();
                 oom.defineClass("Class" + i, code, 0, code.length);
                 j++;
