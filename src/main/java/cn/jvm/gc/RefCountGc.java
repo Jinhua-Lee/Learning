8164ffc6610292fbf06342379c564f8bf58cd945
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-04-29 22:13:47 +0800

    垃圾回收概述，以及垃圾标记阶段的【标记引用算法】【可达性分析算法】基本思路

---
+++ src/main/java/cn/jvm/gc/RefCountGc.java
@@ -0,0 +1,35 @@
+package cn.jvm.gc;
+
+/**
+ * 循环引用问题探究<p>&emsp;
+ * -XX:+PrintGCDetails <p><p>
+ * 操作：打开或关闭{@link System#gc()} 方法；<p>
+ * 结果：GC信息，内存占用不同；<p>
+ * 结论：Java未使用引用计数算法；<p>
+ *
+ * @author Jinhua
+ * @version 1.0
+ * @date 2021/4/29 21:04
+ */
+public class RefCountGc {
+    /**
+     * 唯一作用：占内存
+     */
+    private final byte[] bigSize = new byte[5 * 2024 * 1024];
+
+    RefCountGc reference = null;
+
+    public static void main(String[] args) {
+        RefCountGc r1 = new RefCountGc();
+        RefCountGc r2 = new RefCountGc();
+
+        // 互相引用
+        r1.reference = r2;
+        r2.reference = r1;
+        // 释放外部引用
+        r1 = null;
+        r2 = null;
+        // 打开或关闭gc方法，观察是否会执行GC
+        System.gc();
+    }
+}
