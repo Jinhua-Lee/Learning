6197d27a6aaa274c1c4b41e1aea534230ac6a8cf
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-07-07 19:40:35 +0800

    hotspot vm中，计算过hash的对象锁不会升级为偏向锁

--- src/main/java/cn/mythread/lock/status/BiasedLock.java
+++ src/main/java/cn/mythread/lock/status/BiasedLock.java
@@ -14,10 +14,13 @@ public class BiasedLock {
     private static Object object;
 
     @SneakyThrows
+    @SuppressWarnings("all")
     public static void main(String[] args) {
         // 偏向锁会有一个延迟，程序刚启动的三四秒内不会出现偏向锁
         Thread.sleep(5_000L);
         object = new Object();
+        // HotSpot VM中，计算过hash值的对象，不会升级为偏向锁，而是直接升级轻量级锁
+        int hashCode = object.hashCode();
         System.out.println(ClassLayout.parseInstance(object).toPrintable());
         lock();
         System.out.println(ClassLayout.parseInstance(object).toPrintable());
