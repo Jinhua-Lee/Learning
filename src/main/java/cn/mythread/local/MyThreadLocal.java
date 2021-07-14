f3d73fe780ef8a515f5b1fd9feb956e957bc9b38
Jinhua-Lee <Jinhua-Lee@outlook.com>
2021-07-14 22:03:38 +0800

    ThreadLocal测试

--- src/main/java/cn/mythread/local/MyThreadLocal.java
+++ src/main/java/cn/mythread/local/MyThreadLocal.java
@@ -9,19 +9,9 @@ import lombok.SneakyThrows;
  * @version 1.0
  * @date 2021/7/14 20:56
  */
-public class MyThreadLocalDemo implements Runnable {
+public class MyThreadLocal {
 
     private static final ThreadLocal<String> LOCAL = new ThreadLocal<>();
-
-    @Override
-    public void run() {
-        LOCAL.set(Thread.currentThread().getName());
-        // 输出：各自线程名
-        System.out.println(Thread.currentThread().getName() + "获取：" + LOCAL.get());
-        LOCAL.remove();
-        // null
-        System.out.println(Thread.currentThread().getName() + "移除后获取：" + LOCAL.get());
-    }
 
     @SneakyThrows
     @SuppressWarnings("all")
@@ -29,11 +19,18 @@ public class MyThreadLocalDemo implements Runnable {
         // 主线程设置，新建线程设置并移除，观察是否受影响
         LOCAL.set(Thread.currentThread().getName());
 
-        Thread t1 = new Thread(new MyThreadLocalDemo(), "线程1");
+        Thread t1 = new Thread(() -> {
+            LOCAL.set(Thread.currentThread().getName());
+            // 输出：线程1
+            System.out.println(Thread.currentThread().getName() + "获取：" + LOCAL.get());
+            LOCAL.remove();
+            // null
+            System.out.println(Thread.currentThread().getName() + "移除后获取：" + LOCAL.get());
+        }, "线程1");
         t1.start();
         // t1线程结束后开始执行获取和移除
-
         t1.join();
+
         // 输出：main
         System.out.println(Thread.currentThread().getName() + "获取：" + LOCAL.get());
         LOCAL.remove();
