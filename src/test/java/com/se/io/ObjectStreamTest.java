bf8db416d0f7b3ec8d3dd5db8c759511774abe39
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-05-07 09:22:39 +0800

    1. 完成对象流调试，包括输入输出、transient关键字。

--- src/test/java/com/se/io/ObjectStreamTest.java
+++ src/test/java/com/se/io/ObjectStreamTest.java
@@ -19,14 +19,15 @@ import java.io.*;
 @Slf4j
 public class ObjectStreamTest {
 
-    private ObjectOutputStream objOutStream;
-    private ObjectInputStream objInStream;
+    private static ObjectOutputStream objOutStream;
+    private static ObjectInputStream objInStream;
+    private static ObjectMapper mapper;
 
-    @BeforeEach
+    @BeforeAll
     @SneakyThrows
     @DisplayName(value = "初始化流绑定的文件")
     @SuppressWarnings("all")
-    public void beforeEach() {
+    public static void beforeEach() {
         String objFilePath = "D:/io_test/object_test.txt";
         File objFile = new File(objFilePath);
 
@@ -38,6 +39,9 @@ public class ObjectStreamTest {
 
         objOutStream = new ObjectOutputStream(new FileOutputStream(objFile));
         objInStream = new ObjectInputStream(new FileInputStream(objFile));
+
+        mapper = new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
     @Test
@@ -47,10 +51,13 @@ public class ObjectStreamTest {
     public void testOutputObj() {
         StudentSerial ljh = new StudentSerial("ljh", "9569");
         StudentSerial lwk = new StudentSerial("lwk", "6868");
+        log.info("out第一个对象：\n{}", mapper.writeValueAsString(ljh));
+        log.info("out第二个对象：\n{}", mapper.writeValueAsString(lwk));
 
         objOutStream.writeObject(ljh);
         objOutStream.writeObject(lwk);
         objOutStream.flush();
+        objOutStream.close();
     }
 
     @Test
@@ -60,19 +67,18 @@ public class ObjectStreamTest {
     public void testInputObj() {
         StudentSerial ljh = (StudentSerial) objInStream.readObject();
         StudentSerial lwk = (StudentSerial) objInStream.readObject();
-
-        ObjectMapper mapper = new ObjectMapper()
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objInStream.close();
 
 
-        log.info("第一个对象：\n{}", mapper.writeValueAsString(ljh));
-        log.info("第二个对象：\n{}", mapper.writeValueAsString(lwk));
+        log.info("in第一个对象：\n{}", mapper.writeValueAsString(ljh));
+        log.info("in第二个对象：\n{}", mapper.writeValueAsString(lwk));
     }
 
-    @AfterEach
+    @AfterAll
     @SneakyThrows
     @DisplayName(value = "关闭流")
-    public void afterAll() {
+    public static void afterAll() {
         objOutStream.close();
+        objInStream.close();
     }
 }
