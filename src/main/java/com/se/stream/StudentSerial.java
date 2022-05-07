685976d3d6e08c84050e025850deb5ff7062fb08
Jinhua-Lee <Jinhua-Lee@outlook.com>
2022-05-07 09:22:39 +0800

    1. 完成对象流调试，包括输入输出、transient关键字。

--- src/main/java/com/se/stream/StudentSerial.java
+++ src/main/java/com/se/stream/StudentSerial.java
@@ -1,13 +1,19 @@
 package com.se.stream;
 
-import java.io.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.io.Serial;
+import java.io.Serializable;
 
 /**
  * 模拟序列化和反序列化
  *
  * @author Jinhua
  */
-public record StudentSerial(String name, String password) implements Serializable {
+@Getter
+@EqualsAndHashCode(of = {"name", "password"})
+public final class StudentSerial implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -16,42 +22,19 @@ public record StudentSerial(String name, String password) implements Serializabl
      * 静态常量
      */
     private static int count = 0;
+    private final String name;
+    private final String password;
+
+    /**
+     * transient关键字标记的属性不进行序列化
+     */
+    private final transient int transFlag;
 
     public StudentSerial(String name, String password) {
         System.out.println("进入构造方法");
         this.name = name;
         this.password = password;
+        transFlag = -1;
         count++;
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        String dir = "D:/IOTest/";
-        String filename = "StudentSerial.txt";
-        File f = new File(dir, filename);
-        if (!f.exists()) {
-            if (!f.createNewFile()) {
-                return;
-            }
-        }
-        StudentSerial s1 = new StudentSerial("李金华", "5206");
-        StudentSerial s2 = new StudentSerial("李文凯", "6868");
-
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
-        oos.writeObject(s1);
-        oos.writeObject(s2);
-        oos.flush();
-        oos.close();
-
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
-        try {
-            StudentSerial s3 = (StudentSerial) ois.readObject();
-            StudentSerial s4 = (StudentSerial) ois.readObject();
-            System.out.println(s3.toString());
-            System.out.println(s4.toString());
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        ois.close();
     }
 }
