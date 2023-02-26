8b83ddf7f8588beaf0e3c98bc87ea2527141af3b
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-02-26 21:10:17 +0800

    [style] 格式调整.

--- src/main/java/cn/io/nio/NioServer.java
+++ src/main/java/cn/io/nio/NioServer.java
@@ -25,43 +25,45 @@ public class NioServer {
 
         // 2. 【通道】
         //      2.1 打开【通道】
-        ServerSocketChannel serverChannel = ServerSocketChannel.open();
-        //      2.2 非阻塞模式
-        serverChannel.configureBlocking(false);
-        //      2.3 绑定服务端口
-        serverChannel.bind(new InetSocketAddress(8082));
+        try (ServerSocketChannel serverChannel = ServerSocketChannel.open()) {
+            //      2.2 非阻塞模式
+            serverChannel.configureBlocking(false);
+            //      2.3 绑定服务端口
+            serverChannel.bind(new InetSocketAddress(8082));
 
-        // 3. 【通道】都注册到【选择器】
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-        // 4. 【选择器】轮询
-        while (selector.select() > 0) {
+            // 3. 【通道】都注册到【选择器】
+            serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+            // 4. 【选择器】轮询
+            while (selector.select() > 0) {
 
-            Iterator<SelectionKey> itr = selector.selectedKeys().iterator();
-            while (itr.hasNext()) {
-                SelectionKey sKey = itr.next();
+                Iterator<SelectionKey> itr = selector.selectedKeys().iterator();
+                while (itr.hasNext()) {
+                    SelectionKey sKey = itr.next();
 
-                // 接受事件，做选择键的初始化
-                if (sKey.isAcceptable()) {
-                    // 连接事件，需要产生一个与客户端对应的channel
-                    SocketChannel accept = serverChannel.accept();
-                    accept.configureBlocking(false);
-                    // 【客户端通道】注册到【选择器】
-                    accept.register(selector, SelectionKey.OP_READ);
+                    // 接受事件，做选择键的初始化
+                    if (sKey.isAcceptable()) {
+                        // 连接事件，需要产生一个与客户端对应的channel
+                        SocketChannel accept = serverChannel.accept();
+                        accept.configureBlocking(false);
+                        // 【客户端通道】注册到【选择器】
+                        accept.register(selector, SelectionKey.OP_READ);
 
-                    // 读事件，通过选择键，拿它channel中的数据。
-                } else if (sKey.isReadable()) {
-                    // 可读事件，则从选择键中拿取Channel就可以了
-                    SocketChannel channel = (SocketChannel) sKey.channel();
+                        // 读事件，通过选择键，拿它channel中的数据。
+                    } else if (sKey.isReadable()) {
+                        // 可读事件，则从选择键中拿取Channel就可以了
+                        try (SocketChannel channel = (SocketChannel) sKey.channel()) {
 
-                    ByteBuffer bBuff = ByteBuffer.allocate(1024);
-                    int len;
-                    while ((len = channel.read(bBuff)) > 0) {
-                        bBuff.flip();
-                        log.info("[nio server] receive client data: {}", new String(bBuff.array(), 0, len));
-                        bBuff.clear();
+                            ByteBuffer bBuff = ByteBuffer.allocate(1024);
+                            int len;
+                            while ((len = channel.read(bBuff)) > 0) {
+                                bBuff.flip();
+                                log.info("[nio server] receive client data: {}", new String(bBuff.array(), 0, len));
+                                bBuff.clear();
+                            }
+                        }
                     }
+                    itr.remove();
                 }
-                itr.remove();
             }
         }
     }
