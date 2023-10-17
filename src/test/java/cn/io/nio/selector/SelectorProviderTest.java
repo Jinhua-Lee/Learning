3cf2bcb6781d5a3e1dc15a1a8447aa0ce6be8ad1
Jinhua-Lee <Jinhua-Lee@outlook.com>
2023-10-17 11:46:25 +0800

    [chore] something about os judgement.

--- src/test/java/cn/io/nio/selector/SelectorProviderTest.java
+++ src/test/java/cn/io/nio/selector/SelectorProviderTest.java
@@ -1,5 +1,6 @@
 package cn.io.nio.selector;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.jupiter.api.*;
 // import sun.nio.ch.EPollSelectorProvider;
 
@@ -21,9 +22,16 @@ public class SelectorProviderTest {
     @DisplayName(value = "通过【系统属性】配置，实例化SelectorProvider")
     public void testSystemPropertyConf() throws IOException {
         String providerKey = "java.nio.channels.spi.SelectorProvider";
-        // 默认是 WEPollSelectorProvider
-        String providerVal = "sun.nio.ch.EPollSelectorProvider";
-        System.setProperty(providerKey, providerVal);
+
+        if (SystemUtils.IS_OS_LINUX){
+            // Linux 默认是 EPollSelectorProvider
+            String providerVal = "sun.nio.ch.PollSelectorProvider";
+            System.setProperty(providerKey, providerVal);
+        } else {
+            // Windows 默认是 WEPollSelectorProvider
+            String providerVal = "sun.nio.ch.WindowsSelectorProvider";
+            System.setProperty(providerKey, providerVal);
+        }
 
         Selector selector = Selector.open();
         SelectorProvider provider = selector.provider();
